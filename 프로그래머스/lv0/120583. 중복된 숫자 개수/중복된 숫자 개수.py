d80f39d8308564f24aafae3d4182017a503276b8
2mook2 <127025513+2mook2@users.noreply.github.com>
2023-04-10 08:08:10 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.py
+++ 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.py
@@ -1,2 +1,8 @@
+from collections import Counter
+
 def solution(array, n):
-    return array.count(n)
+    if Counter(array).get(n):
+        answer = Counter(array).get(n)
+    else:
+        answer = 0
+    return answer
