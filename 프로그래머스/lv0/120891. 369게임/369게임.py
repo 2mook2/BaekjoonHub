94b1c337bd3b8a48888c498bafa61d59c1df5210
2mook2 <127025513+2mook2@users.noreply.github.com>
2023-04-14 11:12:35 +0900

    [level 0] Title: 369게임, Time: 0.08 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.py
+++ 프로그래머스/lv0/120891. 369게임/369게임.py
@@ -1,6 +1,14 @@
+# def solution(order):
+#     count = 0
+#     count += str(order).count('3')
+#     count += str(order).count('6')
+#     count += str(order).count('9')
+#     return count
+
+# import re
+# re.sub('[^369]', '', '123123efjdksajfa123123f6699999')
+# re.findall('[369]', '3398764819731')
+
+import re
 def solution(order):
-    count = 0
-    count += str(order).count('3')
-    count += str(order).count('6')
-    count += str(order).count('9')
-    return count
+    return len(re.findall('[369]', str(order)))
