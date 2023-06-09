8c2301fa16cad000a5aa35b6be1c5ed473153012
2mook2 <127025513+2mook2@users.noreply.github.com>
2023-06-10 00:53:48 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -0,0 +1,11 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+         'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+word = input()
+
+for i in alphabet:
+    if i in word:
+        idx = word.index(i)
+    else:
+        idx = -1    
+    print(idx)
