395ba41bf493e59a003bdb5f47ad2c59c6576457
2mook2 <127025513+2mook2@users.noreply.github.com>
2023-06-11 23:16:43 +0900

    [Silver IV] Title: 수 찾기, Time: 184 ms, Memory: 56308 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,14 +1,14 @@
 # BOJ 1920번
 N = int(input())
-A = set((map(int, input().split(' '))))
+A = list(map(int, input().split(' ')))
 
 M = int(input())
 X = list(map(int, input().split(' ')))
 
-result = set(X) - A
+존재하지않음 = set(X) - set(A)
 
 for i in X:
-    if i in result:
+    if i in 존재하지않음:
         print(0)
     else:
         print(1)
