3ff5b910ff613607ffef387f4510db704a4fcedb
2mook2 <127025513+2mook2@users.noreply.github.com>
2023-06-13 18:08:18 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1528 ms, Memory: 77108 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+
+result = []
+while N > 0:
+    x = int(input())
+    result.append(x)
+    N -= 1
+
+result.sort()
+for i in result:
+    print(i)
