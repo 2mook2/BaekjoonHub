9e8dc0cb9ab298b244dd32e9b91b0fec889aa6d7
2mook2 <127025513+2mook2@users.noreply.github.com>
2023-06-15 22:10:30 +0900

    [Silver V] Title: 나이순 정렬, Time: 4196 ms, Memory: 46276 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+info = []
+for i in range(N):
+    age, name = input().split()
+    info.append((int(age), name))
+    
+info.sort(key=lambda x: x[0])
+for j in info:
+    print(j[0], end=' ')
+    print(j[1])
