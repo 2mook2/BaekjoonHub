682aa7b764ac37bccda1ee2d42e7faf876851633
2mook2 <127025513+2mook2@users.noreply.github.com>
2023-06-17 16:13:24 +0900

    [Bronze II] Title: 문자열 반복, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -0,0 +1,12 @@
+data = int(input())
+
+problems = []
+for i in range(data):
+  problem = input().split(' ')
+  problems.append(problem)
+
+for i in problems:
+  for j in i[1]:
+    print(j * int(i[0]), end='')
+  if j == i[1][-1]:
+    print()
