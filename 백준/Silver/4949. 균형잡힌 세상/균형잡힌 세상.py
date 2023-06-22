db7d3390e9a61d492b30d7eca01f63ec726d2e2d
2mook2 <127025513+2mook2@users.noreply.github.com>
2023-06-22 16:24:26 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 432 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,25 @@
+while True:
+    sentence = input().rstrip()
+    stack = []
+
+    if sentence == '.':
+        break
+
+    for char in sentence:
+        if char == '(' or char == '[':
+            stack.append(char)
+        elif char == ')':
+            if stack and stack[-1] == '(':
+                stack.pop()
+            else:
+                stack.append(char)
+        elif char == ']':
+            if stack and stack[-1] == '[':
+                stack.pop()
+            else:
+                stack.append(char)
+
+    if stack:
+        print('no')
+    else:
+        print('yes')
