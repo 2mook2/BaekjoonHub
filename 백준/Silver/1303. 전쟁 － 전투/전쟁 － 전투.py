ecb920edecd29f5207205a251071b614aa4c4f1e
2mook2 <127025513+2mook2@users.noreply.github.com>
2023-07-06 21:12:45 +0900

    [Silver I] Title: 전쟁 - 전투, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.py
@@ -0,0 +1,36 @@
+# BOJ 1303번
+n, m = map(int, input().split())
+
+graph = [list(input()) for _ in range(m)]
+
+visited = [[0] * n for _ in range(m)]
+
+def dfs(x, y, color):
+    global cnt
+    if x < 0 or x >= m or y < 0 or y >= n:
+        return False
+    
+    if not visited[x][y] and graph[x][y] == color:
+        visited[x][y] = 1
+        cnt += 1
+        dfs(x - 1, y, color)
+        dfs(x + 1, y, color)
+        dfs(x, y - 1, color)
+        dfs(x, y + 1, color)
+        return True
+    return True
+
+result_w = 0
+result_b = 0
+for i in range(m):
+    for j in range(n):
+        cnt = 0
+        if not visited[i][j] and dfs(i, j, 'W'):
+            result_w += cnt ** 2
+        
+        cnt = 0
+        if not visited[i][j] and dfs(i, j, 'B'):
+            result_b += cnt ** 2
+
+print(result_w, result_b)
+# print(visited)
