99d3594a41fe14f4722be3d243e00c0d7b7fa36f
2mook2 <127025513+2mook2@users.noreply.github.com>
2023-07-24 19:32:04 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 6224 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,20 +1,14 @@
+# BOJ 10989번
 import sys
-
-# 입력과 출력을 모두 sys.stdin, sys.stdout를 이용하여 처리
 input = sys.stdin.readline
 print = sys.stdout.write
-
 n = int(input().rstrip())
 
-# 10,000 이하의 자연수를 카운팅할 배열
-count = [0] * 10001
+cnt = [0] * 10001
 
 for i in range(n):
-    x = int(input().rstrip())
-    count[x] += 1
+    cnt[int(input())] += 1
 
-# 배열에 저장된 수만큼 해당 인덱스(수)를 출력
-for num in range(1, 10001):
-    if count[num] > 0:
-        for _ in range(count[num]):
-            print(f"{num}\n")
+for i in range(10001):
+    for j in range(cnt[i]):
+        print(f'{i}\n')
