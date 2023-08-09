39a2d20790a77512c889da086d02ef38d13d3600
2mook2 <127025513+2mook2@users.noreply.github.com>
2023-08-09 23:52:59 +0900

    [Silver V] Title: 최소공배수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/13241. 최소공배수/최소공배수.py
+++ 백준/Silver/13241. 최소공배수/최소공배수.py
@@ -3,12 +3,7 @@ a, b = map(int, input().split())
 
 a, b = min(a, b), max(a, b)
 
-b의배수 = []
-
 for i in range(1, a + 1):
-    b의배수.append(b * i)
-
-for i in b의배수:
-    if not i % a:
-        print(i)
+    if not (b * i) % a:
+        print(b * i)
         break
