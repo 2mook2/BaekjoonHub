5f5589e2adecf23c84298939c092881135ed3ba7
2mook2 <127025513+2mook2@users.noreply.github.com>
2023-08-23 03:15:59 +0900

    [Silver III] Title: 통계학, Time: 512 ms, Memory: 57084 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -1,24 +1,31 @@
 # BOJ 2108번
 import sys
+from collections import defaultdict
 input = sys.stdin.readline
 
 n = int(input().rstrip())
 
 arr = []
-cnt = [0] * 8001 # -4000부터 4000까지 정수 카운트 (문제 범위)
+dictionary = defaultdict(int)
 for _ in range(n):
     x = int(input().rstrip())
     arr.append(x)
-    cnt[x + 4000] += 1 # 음수가 들어왔을 경우 ex) -2 -> 3998에 기록
+    dictionary[x] += 1
 
 arr = sorted(arr)
-max_cnt = [i - 4000 for i in range(8001) if cnt[i] == max(cnt)] # 최빈값만 저장
+max_cnt = max(dictionary.values())
+max_cnts = []
+for key, val in dictionary.items():
+    if val == max_cnt:
+        max_cnts.append(key)
+
+max_cnts = sorted(max_cnts)
 
 # mean
-print(int(round(sum(arr) / n, 0)))
+print(round(sum(arr) / n))
 # median
 print(arr[n//2])
 # mode
-print(max_cnt[0] if len(max_cnt) == 1 else max_cnt[1])
+print(max_cnts[0] if len(max_cnts) == 1 else max_cnts[1])
 # range (max - min)
 print(arr[-1] - arr[0])
